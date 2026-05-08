3a6545848116495db38890af11129ba7beffa2fc
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-08 20:53:31 +0700

    Add UI basic structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,131 @@
-import Image from "next/image";
+const weatherData = {
+  location: {
+    name: "Garut",
+    country: "Indonesia",
+    latitude: -6.91,
+    longitude: 107.61,
+    timezone: "Asia/Jakarta",
+  },
+
+  current: {
+    time: "2026-05-13T02:00",
+
+    temperature: 24,
+    feelsLike: 26,
+
+    humidity: 80,
+    windSpeed: 5,
+
+    weatherCode: 3,
+
+    isDay: true,
+  },
+
+  hourly: [
+    {
+      time: "2026-05-13T03:00",
+
+      temperature: 24,
+
+      humidity: 82,
+
+      weatherCode: 3,
+    },
+  ],
+
+  daily: [
+    {
+      date: "2026-05-13",
+
+      minTemperature: 21,
+      maxTemperature: 29,
+
+      weatherCode: 2,
+
+      sunrise: "2026-05-13T05:48",
+      sunset: "2026-05-13T17:52",
+    },
+  ],
+};
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex">
+      {/* left part */}
+      <div className="bg-blue-100 w-80">
+        {/* search */}
+        <div>main search</div>
+
+        {/* main part */}
+        <div>
+          {/* main section */}
+          <div>
+            <div>28</div>
+            <div>Rainy Day</div>
+            <div>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Nesciunt, sed. Sequi quis at,
+            </div>
+          </div>
+
+          {/* detail grid */}
+          <section className="grid grid-cols-2 gap-4 ">
+            {Array.from({ length: 4 }).map((_, i) => (
+              <div key={i} className="border">
+                <div>FEELS LIKE</div>
+                <div>30</div>
+                <p>lorem ipsum dolor sit</p>
+              </div>
+            ))}
+          </section>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      {/* right part */}
+      <div className="bg-red-100 flex-1 overflow-hidden grid grid-cols-2 gap-6">
+        {/* hourly forecast */}
+        <div className="border col-span-2">
+          <h3>HOURLY FORECAST</h3>
+          <div className="flex gap-2">
+            {Array.from({ length: 15 }).map((_, i) => (
+              <div key={i} className="border">
+                <div>Now</div>
+                <div>28</div>
+                <p>icon</p>
+              </div>
+            ))}
+          </div>
         </div>
-      </main>
+
+        {/* hourly forecast */}
+        <div className="border  col-span-2">
+          <h3>DAILY FORECAST</h3>
+          <div className="flex gap-2">
+            {Array.from({ length: 15 }).map((_, i) => (
+              <div key={i} className="border">
+                <div>Now</div>
+                <div>28</div>
+                <p>icon</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* other details */}
+        <div className="border">
+          <h3>uv index</h3>
+          <div>3</div>
+          <p>moderate</p>
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+
+        <div className="border">
+          <h3>wind</h3>
+          <div>3</div>
+          <p>moderate</p>
+          <p>lorem ipsum dolor sit amet</p>
+        </div>
+      </div>
     </div>
   );
 }
