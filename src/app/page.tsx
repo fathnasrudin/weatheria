f0568a9d214eb60891c1c149976d92e927a79fa3
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-09 11:25:53 +0700

    Add ui spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,18 +50,18 @@ const weatherData = {
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className="flex gap-4 p-6">
       {/* left part */}
-      <div className="bg-blue-100 w-80">
-        {/* search */}
-        <div>main search</div>
+      <div className="flex flex-col gap-4 w-80">
+        {/* searchbar */}
+        <div className="bg-red-200 py-1 px-2 rounded-full">searchbar</div>
 
         {/* main part */}
-        <div>
+        <div className="bg-red-200 rounded-2xl p-2">
           {/* main section */}
-          <div>
-            <div>28</div>
-            <div>Rainy Day</div>
+          <div className="text-center py-16">
+            <div className="text-6xl">28</div>
+            <div className="text-2xl">Rainy Day</div>
             <div>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Nesciunt, sed. Sequi quis at,
@@ -69,9 +69,9 @@ export default function Home() {
           </div>
 
           {/* detail grid */}
-          <section className="grid grid-cols-2 gap-4 ">
+          <section className="grid grid-cols-2 gap-2 ">
             {Array.from({ length: 4 }).map((_, i) => (
-              <div key={i} className="border">
+              <div key={i} className="border p-2 rounded-xl">
                 <div>FEELS LIKE</div>
                 <div>30</div>
                 <p>lorem ipsum dolor sit</p>
@@ -82,13 +82,13 @@ export default function Home() {
       </div>
 
       {/* right part */}
-      <div className="bg-red-100 flex-1 overflow-hidden grid grid-cols-2 gap-6">
+      <div className="flex-1 overflow-hidden grid grid-cols-2 auto-rows-min gap-4">
         {/* hourly forecast */}
-        <div className="border col-span-2">
+        <div className="bg-red-200 border col-span-2 rounded-2xl p-2">
           <h3>HOURLY FORECAST</h3>
-          <div className="flex gap-2">
+          <div className="flex gap-2  overflow-hidden">
             {Array.from({ length: 15 }).map((_, i) => (
-              <div key={i} className="border">
+              <div key={i} className="border p-1 rounded-xl text-center">
                 <div>Now</div>
                 <div>28</div>
                 <p>icon</p>
@@ -98,11 +98,11 @@ export default function Home() {
         </div>
 
         {/* hourly forecast */}
-        <div className="border  col-span-2">
+        <div className="bg-red-200 border  col-span-2  rounded-2xl p-2">
           <h3>DAILY FORECAST</h3>
-          <div className="flex gap-2">
+          <div className="flex gap-2 overflow-hidden">
             {Array.from({ length: 15 }).map((_, i) => (
-              <div key={i} className="border">
+              <div key={i} className="border p-1 rounded-xl text-center">
                 <div>Now</div>
                 <div>28</div>
                 <p>icon</p>
@@ -112,14 +112,14 @@ export default function Home() {
         </div>
 
         {/* other details */}
-        <div className="border">
+        <div className="bg-red-200 border rounded-2xl p-2">
           <h3>uv index</h3>
           <div>3</div>
           <p>moderate</p>
           <p>lorem ipsum dolor sit amet</p>
         </div>
 
-        <div className="border">
+        <div className="bg-red-200 border rounded-2xl p-2">
           <h3>wind</h3>
           <div>3</div>
           <p>moderate</p>
