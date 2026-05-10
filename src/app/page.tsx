00544e5d8d5a7bd515424916c6ea7a9d4072e889
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-10 14:31:26 +0700

    Add search location feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 
 const weatherData: IWeatherData = {
   location: {
@@ -21,7 +21,6 @@ const weatherData: IWeatherData = {
 
     weatherCode: 3,
 
-    details: {},
     isDay: true,
   },
 
@@ -163,14 +162,35 @@ interface IWeatherData {
   }[];
 }
 
-function normalizeOpenMeteo(rawForecast: IOpenMeteoForecast) {
+interface IGeocodingItem {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  elevation: number;
+  feature_code: string;
+  country_code: string;
+  admin1_id: number;
+  admin2_id: number;
+  timezone: string;
+  population: number;
+  country_id: number;
+  country: string;
+  admin1: string;
+  admin2: string;
+}
+
+function normalizeOpenMeteo(
+  rawForecast: IOpenMeteoForecast,
+  geolocation: IGeocodingItem,
+) {
   return {
     location: {
-      name: "unknown",
-      country: "unknown",
-      latitude: rawForecast.latitude,
-      longitude: rawForecast.longitude,
-      timezone: rawForecast.timezone,
+      name: `${geolocation.name}, ${geolocation.admin2}, ${geolocation.admin1}`,
+      country: geolocation.country,
+      latitude: geolocation.latitude,
+      longitude: geolocation.longitude,
+      timezone: geolocation.timezone,
     },
 
     current: {
@@ -208,119 +228,186 @@ function normalizeOpenMeteo(rawForecast: IOpenMeteoForecast) {
   };
 }
 
-export default function Home() {
-  const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("");
+const fetchGeocoding = async (searchLocation: string) => {
+  try {
+    const response = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${searchLocation}&count=3&language=en&format=json`,
+    );
 
-  useEffect(() => {
-    async function fetchWeatherData() {
-      const lat = -6.4214;
-      const long = 106.7331;
-      try {
-        setLoading(true);
-        setErrorMessage("");
-        const response = await fetch(
-          `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`,
-        );
-
-        if (!response.ok) {
-          throw new Error("Response Not Ok");
-        }
-
-        const result = await response.json();
-        console.log({ result });
-        console.log(normalizeOpenMeteo(result));
-        setWeatherData(normalizeOpenMeteo(result));
-      } catch (error) {
-        console.log(error);
-        setErrorMessage("Something went wrong");
-      } finally {
-        setLoading(false);
-      }
+    if (!response.ok) {
+      throw new Error("Response Not Ok");
     }
 
-    fetchWeatherData();
-  }, []);
+    const result = await response.json();
 
-  if (loading) return <p>Loading...</p>;
-  if (errorMessage) return <p>{errorMessage}</p>;
-  if (!weatherData) return <p>Data not found</p>;
+    const location: IGeocodingItem = result.results[0];
+    if (!location) throw new Error("Location not found");
+
+    return { location, error: null };
+  } catch (error) {
+    console.error(error);
+    return { location: null, error };
+  }
+};
+
+async function fetchWeather({ lat, long }: { lat: number; long: number }) {
+  try {
+    const response = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`,
+    );
+
+    if (!response.ok) {
+      throw new Error("Response Not Ok");
+    }
+
+    const result: IOpenMeteoForecast = await response.json();
+    return { data: result, error: null };
+  } catch (error) {
+    console.error(error);
+    return { data: null, error };
+  }
+}
+
+function Searchbar({
+  setWeatherData,
+}: {
+  setWeatherData: React.Dispatch<SetStateAction<IWeatherData | null>>;
+}) {
+  const [searchLocation, setSearchLocation] = useState("");
+
+  return (
+    <form className="flex gap-4">
+      <input
+        type="text"
+        placeholder="searchbar"
+        className="flex-1 bg-gray-200 py-1 px-2 rounded-full"
+        value={searchLocation}
+        onChange={(e) => {
+          setSearchLocation(e.target.value);
+        }}
+      />
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+
+          const { error, location } = await fetchGeocoding(searchLocation);
+          console.log({ error, location });
+
+          if (!location) {
+            console.error(error);
+            return;
+          }
+
+          const { data: rawForecast, error: fetchWeatherError } =
+            await fetchWeather({
+              lat: location?.latitude,
+              long: location?.longitude,
+            });
+
+          if (fetchWeatherError) {
+            console.error(fetchWeatherError);
+            return;
+          }
+
+          if (!rawForecast) {
+            console.error("Data not found");
+            return;
+          }
+
+          //
+          const normalizedData = normalizeOpenMeteo(rawForecast, location);
+          console.log({ normalizedData });
+          setWeatherData(normalizedData);
+        }}
+        className="cursor-pointer"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default function Home() {
+  const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
 
   return (
     <div className="flex flex-col sm:flex-row gap-4 p-6 bg-gray-100">
       {/* left part */}
       <div className="w-full sm:w-80 flex flex-col gap-4 ">
         {/* searchbar */}
-        <div className="bg-gray-200 py-1 px-2 rounded-full">searchbar</div>
+        <Searchbar setWeatherData={setWeatherData} />
 
-        {/* main part */}
-        <div className="bg-blue-200 rounded-2xl p-4 space-y-16">
-          <div className="text-center text-xl">
-            {weatherData.location.name}, {weatherData.location.country}
-          </div>
-
-          {/* main section */}
-          <div className="text-center">
-            <div className="text-6xl">{weatherData.current.temperature}</div>
-            <div className="text-2xl">
-              weather code {weatherData.current.weatherCode}
+        {weatherData && (
+          <div className="bg-blue-200 rounded-2xl p-4 space-y-16">
+            {/* main part */}
+            <div className="text-center text-xl">
+              {weatherData.location.name}, {weatherData.location.country}
             </div>
-          </div>
 
-          {/* detail grid */}
-          <section className="grid grid-cols-2 gap-2 ">
-            {currentWeatherDetails.map((item) => (
-              <div key={item.title} className="border p-2 rounded-xl">
-                {/* generic title */}
-                <div className="text-xs font-bold text-gray-700">
-                  {item.title}
-                </div>
-                <div className="text-2xl font-bold">{item.value}</div>
-                <p className="text-xs mt-4 text-gray-800">No description</p>
+            {/* main section */}
+            <div className="text-center">
+              <div className="text-6xl">{weatherData.current.temperature}</div>
+              <div className="text-2xl">
+                weather code {weatherData.current.weatherCode}
               </div>
-            ))}
-          </section>
-        </div>
+            </div>
+
+            {/* detail grid */}
+            <section className="grid grid-cols-2 gap-2 ">
+              {currentWeatherDetails.map((item) => (
+                <div key={item.title} className="border p-2 rounded-xl">
+                  {/* generic title */}
+                  <div className="text-xs font-bold text-gray-700">
+                    {item.title}
+                  </div>
+                  <div className="text-2xl font-bold">{item.value}</div>
+                  <p className="text-xs mt-4 text-gray-800">No description</p>
+                </div>
+              ))}
+            </section>
+          </div>
+        )}
       </div>
 
       {/* right part */}
-      <div className="flex-1 overflow-hidden grid grid-cols-2 auto-rows-min gap-4">
-        {/* hourly forecast */}
-        <div className="bg-blue-200 border col-span-2 rounded-2xl p-2 space-y-4">
-          <h3>HOURLY FORECAST</h3>
-          <div className="flex gap-2  overflow-auto">
-            {weatherData.hourly.map((forecast) => (
-              <HourlyForecastCard key={forecast.time} hForecast={forecast} />
-            ))}
+      {weatherData && (
+        <div className="flex-1 overflow-hidden grid grid-cols-2 auto-rows-min gap-4">
+          {/* hourly forecast */}
+          <div className="bg-blue-200 border col-span-2 rounded-2xl p-2 space-y-4">
+            <h3>HOURLY FORECAST</h3>
+            <div className="flex gap-2  overflow-auto">
+              {weatherData.hourly.map((forecast) => (
+                <HourlyForecastCard key={forecast.time} hForecast={forecast} />
+              ))}
+            </div>
+          </div>
+
+          {/* hourly forecast */}
+          <div className="bg-blue-200 border  col-span-2  rounded-2xl p-2">
+            <h3>DAILY FORECAST</h3>
+            <div className="flex gap-2   overflow-auto">
+              {weatherData.daily.map((dforecast) => (
+                <DailyForecastCard key={dforecast.date} dForecast={dforecast} />
+              ))}
+            </div>
+          </div>
+
+          {/* other details */}
+          <div className="bg-blue-200 border rounded-2xl p-2">
+            <h3>uv index</h3>
+            <div>3</div>
+            <p>moderate</p>
+            <p>lorem ipsum dolor sit amet</p>
+          </div>
+
+          <div className="bg-blue-200 border rounded-2xl p-2">
+            <h3>wind</h3>
+            <div>3</div>
+            <p>moderate</p>
+            <p>lorem ipsum dolor sit amet</p>
           </div>
         </div>
-
-        {/* hourly forecast */}
-        <div className="bg-blue-200 border  col-span-2  rounded-2xl p-2">
-          <h3>DAILY FORECAST</h3>
-          <div className="flex gap-2   overflow-auto">
-            {weatherData.daily.map((dforecast) => (
-              <DailyForecastCard key={dforecast.date} dForecast={dforecast} />
-            ))}
-          </div>
-        </div>
-
-        {/* other details */}
-        <div className="bg-blue-200 border rounded-2xl p-2">
-          <h3>uv index</h3>
-          <div>3</div>
-          <p>moderate</p>
-          <p>lorem ipsum dolor sit amet</p>
-        </div>
-
-        <div className="bg-blue-200 border rounded-2xl p-2">
-          <h3>wind</h3>
-          <div>3</div>
-          <p>moderate</p>
-          <p>lorem ipsum dolor sit amet</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
