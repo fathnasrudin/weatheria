cad10316eaf7b25998457fc7161d4b5b07648931
fath-nasrudin <fath.nasrudin.i@gmail.com>
2026-05-12 13:58:25 +0700

    Extract api services

--- src/features/weather/components/search-location.tsx
+++ src/features/weather/components/search-location.tsx
@@ -1,76 +1,6 @@
 "use client";
 import { SetStateAction, useState } from "react";
-import { getWeather } from "../weather.service";
-
-const fetchGeocoding = async (searchLocation: string) => {
-  try {
-    const response = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${searchLocation}&count=3&language=en&format=json`,
-    );
-
-    if (!response.ok) {
-      throw new Error("Response Not Ok");
-    }
-
-    const result = await response.json();
-
-    const location: IGeocodingItem = result.results[0];
-    if (!location) throw new Error("Location not found");
-
-    return { location, error: null };
-  } catch (error) {
-    console.error(error);
-    return { location: null, error };
-  }
-};
-
-function normalizeOpenMeteo(
-  rawForecast: IOpenMeteoForecast,
-  geolocation: IGeocodingItem,
-) {
-  return {
-    location: {
-      name: `${geolocation.name}, ${geolocation.admin2}, ${geolocation.admin1}`,
-      country: geolocation.country,
-      latitude: geolocation.latitude,
-      longitude: geolocation.longitude,
-      timezone: geolocation.timezone,
-    },
-
-    current: {
-      time: rawForecast.current.time,
-
-      temperature: rawForecast.current.temperature_2m,
-      feelsLike: 0,
-
-      humidity: 0,
-      windSpeed: 0,
-
-      weatherCode: 0,
-
-      details: {},
-      isDay: true,
-    },
-
-    hourly: [
-      {
-        time: "2026-05-13T02:00",
-        temperature: 0,
-      },
-      {
-        time: "2026-05-13T03:00",
-        temperature: 0,
-      },
-    ],
-
-    daily: [
-      {
-        date: "2026-05-13",
-        temperature: 0,
-      },
-    ],
-  };
-}
+import { getWeatherByLocation } from "../weather.service";
 
 export function Searchbar({
   setWeatherData,
@@ -93,35 +23,8 @@ export function Searchbar({
       <button
         onClick={async (e) => {
           e.preventDefault();
-
-          const { error, location } = await fetchGeocoding(searchLocation);
-          console.log({ error, location });
-
-          if (!location) {
-            console.error(error);
-            return;
-          }
-
-          const { data: rawForecast, error: fetchWeatherError } =
-            await getWeather({
-              lat: location?.latitude,
-              long: location?.longitude,
-            });
-
-          if (fetchWeatherError) {
-            console.error(fetchWeatherError);
-            return;
-          }
-
-          if (!rawForecast) {
-            console.error("Data not found");
-            return;
-          }
-
-          //
-          const normalizedData = normalizeOpenMeteo(rawForecast, location);
-          console.log({ normalizedData });
-          setWeatherData(normalizedData);
+          const data = await getWeatherByLocation(searchLocation);
+          setWeatherData(data);
         }}
         className="cursor-pointer"
       >
--- src/features/weather/weather.service.ts
+++ src/features/weather/weather.service.ts
@@ -1,3 +1,51 @@
+function normalizeOpenMeteo(
+  rawForecast: IOpenMeteoForecast,
+  geolocation: IGeocodingItem,
+) {
+  return {
+    location: {
+      name: `${geolocation.name}, ${geolocation.admin2}, ${geolocation.admin1}`,
+      country: geolocation.country,
+      latitude: geolocation.latitude,
+      longitude: geolocation.longitude,
+      timezone: geolocation.timezone,
+    },
+
+    current: {
+      time: rawForecast.current.time,
+
+      temperature: rawForecast.current.temperature_2m,
+      feelsLike: 0,
+
+      humidity: 0,
+      windSpeed: 0,
+
+      weatherCode: 0,
+
+      details: {},
+      isDay: true,
+    },
+
+    hourly: [
+      {
+        time: "2026-05-13T02:00",
+        temperature: 0,
+      },
+      {
+        time: "2026-05-13T03:00",
+        temperature: 0,
+      },
+    ],
+
+    daily: [
+      {
+        date: "2026-05-13",
+        temperature: 0,
+      },
+    ],
+  };
+}
+
 export async function getWeather({ lat, long }: { lat: number; long: number }) {
   try {
     const response = await fetch(
@@ -14,4 +62,50 @@ export async function getWeather({ lat, long }: { lat: number; long: number }) {
     console.error(error);
     return { data: null, error };
   }
+}
+
+export async function getWeatherByCoordinate({
+  lat,
+  long,
+}: {
+  lat: number;
+  long: number;
+}) {
+  const response = await fetch(
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Response Not Ok");
+  }
+
+  const result: IOpenMeteoForecast = await response.json();
+  return result;
+}
+
+const getGeocoding = async (searchLocation: string) => {
+  const response = await fetch(
+    `https://geocoding-api.open-meteo.com/v1/search?name=${searchLocation}&count=3&language=en&format=json`,
+  );
+
+  if (!response.ok) {
+    throw new Error("Response Not Ok");
+  }
+
+  const result = await response.json();
+
+  const location: IGeocodingItem = result.results[0];
+  if (!location) throw new Error("Location Not Found");
+  return location;
+};
+
+export async function getWeatherByLocation(locationName: string) {
+  const location = await getGeocoding(locationName);
+  const weather = await getWeatherByCoordinate({
+    lat: location.latitude,
+    long: location.longitude,
+  });
+
+  const normalizedWeather = normalizeOpenMeteo(weather, location);
+  return normalizedWeather;
 }
